a4ddda42fb0e8311a1e279355a8abd4a868a79c3
AzinecLLC <azinecllc@AzinecLLCs-Mac-mini.local>
2017-04-21 16:21:41 +0300

    second

---
+++ TestApplication/HelperDetailsChargingPointViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HelperDetailsChargingPointViewController.swift
+//  TestApplication
+//
+//  Created by AzinecLLC on 4/21/17.
+//  Copyright © 2017 AzinecLLC. All rights reserved.
+//
+
+import UIKit
+
+class HelperDetailsChargingPointViewController: NSObject {
+
+}
